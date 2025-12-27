40621ad36a5cb3b3505e764aea75243430532c96
Bigfoot71 <victor.lejuez@protonmail.ch>
2025-12-27 18:40:22 +0100

    tweak

--- src/modules/r3d_target.c
+++ src/modules/r3d_target.c
@@ -43,7 +43,6 @@ static const target_config_t TARGET_CONFIG[] = {
     [R3D_TARGET_ALBEDO]     = { GL_RGB8,              GL_RGB,             GL_UNSIGNED_BYTE,  1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_NORMAL]     = { GL_RG16F,             GL_RG,              GL_HALF_FLOAT,     1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_ORM]        = { GL_RGB8,              GL_RGB,             GL_UNSIGNED_BYTE,  1.0f, GL_NEAREST,              GL_NEAREST, false },
-    [R3D_TARGET_DEPTH]      = { GL_DEPTH_COMPONENT24, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT,   1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_DIFFUSE]    = { GL_RGBA16F,           GL_RGB,             GL_HALF_FLOAT,     1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_SPECULAR]   = { GL_RGBA16F,           GL_RGB,             GL_HALF_FLOAT,     1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_SSAO_0]     = { GL_R8,                GL_RED,             GL_UNSIGNED_BYTE,  0.5f, GL_LINEAR,               GL_LINEAR,  false },
@@ -54,6 +53,7 @@ static const target_config_t TARGET_CONFIG[] = {
     [R3D_TARGET_BLOOM]      = { GL_RGB16F,            GL_RGB,             GL_HALF_FLOAT,     1.0f, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR,  true  },
     [R3D_TARGET_SCENE_0]    = { GL_RGB16F,            GL_RGB,             GL_HALF_FLOAT,     1.0f, GL_NEAREST,              GL_NEAREST, false },
     [R3D_TARGET_SCENE_1]    = { GL_RGB16F,            GL_RGB,             GL_HALF_FLOAT,     1.0f, GL_NEAREST,              GL_NEAREST, false },
+    [R3D_TARGET_DEPTH]      = { GL_DEPTH_COMPONENT24, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT,   1.0f, GL_NEAREST,              GL_NEAREST, false },
 };
 
 static void target_load(r3d_target_t target)
