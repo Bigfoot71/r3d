Bigfoot71/r3d

commit 2995aa20f9e9949560310182557d41fd10d15151

remove the material outputs (forward)

src/r3d_draw.c
@@ -1404,8 +1404,7 @@ static void pass_scene_forward_send_lights(const r3d_draw_call_t* call)
 
 void pass_scene_forward(r3d_target_t sceneTarget)
 {
-    // NOTE: Material outputs are necessary for certain post-process passes such as SSR
-    R3D_TARGET_BIND(sceneTarget, R3D_TARGET_ALBEDO, R3D_TARGET_NORMAL, R3D_TARGET_ORM, R3D_TARGET_DEPTH);
+    R3D_TARGET_BIND(sceneTarget);
     R3D_SHADER_USE(scene.forward);
 
     glEnable(GL_DEPTH_TEST);

shaders/scene/forward.frag
@@ -99,9 +99,6 @@ const vec2 VOGEL_DISK[8] = vec2[8](
 /* === Fragments === */
 
 layout(location = 0) out vec4 FragColor;
-layout(location = 1) out vec4 FragAlbedo;
-layout(location = 2) out vec4 FragNormal;
-layout(location = 3) out vec4 FragORM;
 
 /* === Shadow functions === */
 
@@ -360,10 +357,4 @@ void main()
     /* Compute the final fragment color */
 
     FragColor = vec4(albedo.rgb * diffuse + specular + emission, albedo.a);
-
-    /* Output material data */
-
-    FragAlbedo = vec4(albedo.rgb, 1.0);
-    FragNormal = vec4(M_EncodeOctahedral(N), vec2(1.0));
-    FragORM = vec4(occlusion, roughness, metalness, 1.0);
 }

commit cdabaea0ba7e5efc2f79e33e7dd1aedeb69827db

fix forward pass broken by: 2995aa20f9e9949560310182557d41fd10d15151

src/r3d_draw.c
@@ -1336,7 +1336,7 @@ static void pass_scene_forward_send_lights(const r3d_draw_call_t* call)
 
 void pass_scene_forward(r3d_target_t sceneTarget)
 {
-    R3D_TARGET_BIND(sceneTarget);
+    R3D_TARGET_BIND(sceneTarget, R3D_TARGET_DEPTH);
     R3D_SHADER_USE(scene.forward);
 
     glEnable(GL_DEPTH_TEST);