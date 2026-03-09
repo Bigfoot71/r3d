c840dc72624e1a69cbf6d2d325d770ed24ce9a88
Bigfoot71 <victor.lejuez@protonmail.ch>
2026-03-09 16:06:46 +0100

    simplify material map loaders

--- src/r3d_material.c
+++ src/r3d_material.c
@@ -7,6 +7,7 @@
  */
 
 #include <r3d/r3d_material.h>
+#include <r3d/r3d_texture.h>
 #include <r3d/r3d_utils.h>
 #include <r3d_config.h>
 
@@ -14,8 +15,6 @@
 #   include "./importer/r3d_importer_internal.h"
 #endif
 
-#include "./modules/r3d_texture.h"
-#include "./common/r3d_image.h"
 #include "./r3d_core_state.h"
 
 // ========================================
@@ -118,214 +117,75 @@ void R3D_UnloadMaterial(R3D_Material material)
 R3D_AlbedoMap R3D_LoadAlbedoMap(const char* fileName, Color color)
 {
     R3D_AlbedoMap map = {0};
-
-    Image image = LoadImage(fileName);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load albedo image: '%s'", fileName);
-        return map;
-    }
-
-    bool srgb = (R3D.colorSpace == R3D_COLORSPACE_SRGB);
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, srgb);
+    map.texture = R3D_LoadTexture(fileName, true);
     map.color = color;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Albedo map loaded successfully: '%s'", fileName);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload albedo texture: '%s'", fileName);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 R3D_AlbedoMap R3D_LoadAlbedoMapFromMemory(const char* fileType, const void* fileData, int dataSize, Color color)
 {
     R3D_AlbedoMap map = {0};
-
-    Image image = LoadImageFromMemory(fileType, fileData, dataSize);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load albedo image from memory (type: '%s')", fileType);
-        return map;
-    }
-
-    bool srgb = (R3D.colorSpace == R3D_COLORSPACE_SRGB);
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, srgb);
+    map.texture = R3D_LoadTextureFromMemory(fileType, fileData, dataSize, true);
     map.color = color;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Albedo map loaded successfully from memory (type: '%s')", fileType);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload albedo texture from memory (type: '%s')", fileType);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 void R3D_UnloadAlbedoMap(R3D_AlbedoMap map)
 {
-    if (map.texture.id == 0 || r3d_texture_is_default(map.texture.id)) {
-        return;
-    }
-
-    UnloadTexture(map.texture);
+    R3D_UnloadTexture(map.texture);
 }
 
 R3D_EmissionMap R3D_LoadEmissionMap(const char* fileName, Color color, float energy)
 {
     R3D_EmissionMap map = {0};
-
-    Image image = LoadImage(fileName);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load emission image: '%s'", fileName);
-        return map;
-    }
-
-    bool srgb = (R3D.colorSpace == R3D_COLORSPACE_SRGB);
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, srgb);
+    map.texture = R3D_LoadTexture(fileName, true);
     map.color = color;
     map.energy = energy;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Emission map loaded successfully: '%s'", fileName);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload emission texture: '%s'", fileName);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 R3D_EmissionMap R3D_LoadEmissionMapFromMemory(const char* fileType, const void* fileData, int dataSize, Color color, float energy)
 {
     R3D_EmissionMap map = {0};
-
-    Image image = LoadImageFromMemory(fileType, fileData, dataSize);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load emission image from memory (type: '%s')", fileType);
-        return map;
-    }
-
-    bool srgb = (R3D.colorSpace == R3D_COLORSPACE_SRGB);
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, srgb);
+    map.texture = R3D_LoadTextureFromMemory(fileType, fileData, dataSize, true);
     map.color = color;
     map.energy = energy;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Emission map loaded successfully from memory (type: '%s')", fileType);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload emission texture from memory (type: '%s')", fileType);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 void R3D_UnloadEmissionMap(R3D_EmissionMap map)
 {
-    if (map.texture.id == 0 || r3d_texture_is_default(map.texture.id)) {
-        return;
-    }
-
-    UnloadTexture(map.texture);
+    R3D_UnloadTexture(map.texture);
 }
 
 R3D_NormalMap R3D_LoadNormalMap(const char* fileName, float scale)
 {
     R3D_NormalMap map = {0};
-
-    Image image = LoadImage(fileName);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load normal map image: '%s'", fileName);
-        return map;
-    }
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, false);
+    map.texture = R3D_LoadTexture(fileName, false);
     map.scale = scale;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Normal map loaded successfully: '%s'", fileName);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload normal map texture: '%s'", fileName);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 R3D_NormalMap R3D_LoadNormalMapFromMemory(const char* fileType, const void* fileData, int dataSize, float scale)
 {
     R3D_NormalMap map = {0};
-
-    Image image = LoadImageFromMemory(fileType, fileData, dataSize);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load normal map image from memory (type: '%s')", fileType);
-        return map;
-    }
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, false);
+    map.texture = R3D_LoadTextureFromMemory(fileType, fileData, dataSize, false);
     map.scale = scale;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "Normal map loaded successfully from memory (type: '%s')", fileType);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload normal map texture from memory (type: '%s')", fileType);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 void R3D_UnloadNormalMap(R3D_NormalMap map)
 {
-    if (map.texture.id == 0 || r3d_texture_is_default(map.texture.id)) {
-        return;
-    }
-
-    UnloadTexture(map.texture);
+    R3D_UnloadTexture(map.texture);
 }
 
 R3D_OrmMap R3D_LoadOrmMap(const char* fileName, float occlusion, float roughness, float metalness)
 {
     R3D_OrmMap map = {0};
-
-    Image image = LoadImage(fileName);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load ORM image: '%s'", fileName);
-        return map;
-    }
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, false);
+    map.texture = R3D_LoadTexture(fileName, false);
     map.occlusion = occlusion;
     map.roughness = roughness;
     map.metalness = metalness;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "ORM map loaded successfully: '%s'", fileName);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload ORM texture: '%s'", fileName);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
@@ -333,35 +193,14 @@ R3D_OrmMap R3D_LoadOrmMapFromMemory(const char* fileType, const void* fileData, 
                                     float occlusion, float roughness, float metalness)
 {
     R3D_OrmMap map = {0};
-
-    Image image = LoadImageFromMemory(fileType, fileData, dataSize);
-    if (!IsImageValid(image)) {
-        R3D_TRACELOG(LOG_WARNING, "Failed to load ORM image from memory (type: '%s')", fileType);
-        return map;
-    }
-
-    map.texture = r3d_image_upload(&image, R3D.textureWrap, R3D.textureFilter, false);
+    map.texture = R3D_LoadTextureFromMemory(fileType, fileData, dataSize, false);
     map.occlusion = occlusion;
     map.roughness = roughness;
     map.metalness = metalness;
-
-    if (map.texture.id != 0) {
-        R3D_TRACELOG(LOG_INFO, "ORM map loaded successfully from memory (type: '%s')", fileType);
-    }
-    else {
-        R3D_TRACELOG(LOG_WARNING, "Failed to upload ORM texture from memory (type: '%s')", fileType);
-    }
-
-    UnloadImage(image);
-
     return map;
 }
 
 void R3D_UnloadOrmMap(R3D_OrmMap map)
 {
-    if (map.texture.id == 0 || r3d_texture_is_default(map.texture.id)) {
-        return;
-    }
-
-    UnloadTexture(map.texture);
+    R3D_UnloadTexture(map.texture);
 }
